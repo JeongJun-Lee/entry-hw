dee4b5272b2597ed4e76746ff7c6ea77d35f19b5
JJ Lee <comseong@gmail.com>
2021-08-24 13:58:20 +0500

    Fix wrong merge rebase from upstream

--- app/src/main/mainRouter.ts
+++ app/src/main/mainRouter.ts
@@ -503,7 +503,7 @@ class MainRouter {
 
         const driverFullPath = path.join(directoryPaths.driver(), driverPath);
         logger.info(`execute driver requested. filePath : ${driverFullPath}`);
-        shell.openItem(driverFullPath);
+        shell.openPath(driverFullPath);
     }
 
     /**
