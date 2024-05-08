ff1196d9771b8160ce341255a94ea19fe4ea054d
JJ Lee <comseong@gmail.com>
2024-05-08 09:49:03 +0500

    Add new feature of irRemote block in ArduExt

--- app/modules/arduinoExt.js
+++ app/modules/arduinoExt.js
@@ -12,9 +12,11 @@ function Module() {
         ULTRASONIC: 7,
         TIMER: 8,
         STEPPER: 9,
-        DHTTEMP: 10,
-        DHTHUMI: 11,
-        DHTINIT: 12,
+        DHTINIT: 10,
+        DHTTEMP: 11,
+        DHTHUMI: 12,
+        IRRINIT: 13,
+        IRREMOTE: 14,
     };
 
     this.actionTypes = {
@@ -35,6 +37,7 @@ function Module() {
         ULTRASONIC: 0,
         DHTTEMP: 0,
         DHTHUMI: 0,
+        IRREMOTE:0,
         DIGITAL: {
             '0': 0,
             '1': 0,
@@ -93,11 +96,13 @@ Module.prototype.setSerialPort = function(sp) {
 Module.prototype.requestInitialData = function() {
     this.isNewConn = true;
     this.makeOutputBuffer(this.sensorTypes.RESET, 0, 0); // 최초 연결시, 하드웨어 초기화 수행
-    return this.makeSensorReadBuffer(this.sensorTypes.ANALOG, 0);
+    // slave mode라서 hw에서 신호를 받아야 연결성립 Analog 0번 port를 활성화
+    return this.makeSensorReadBuffer(this.sensorTypes.ANALOG, 0, 0); 
 };
 
 // 연결 후 초기에 수신받아서 정상연결인지를 확인해야하는 경우 사용합니다.
 Module.prototype.checkInitialData = function(data, config) {
+    console.error('Initial packet arrived!!!');
     return true;
 };
 
@@ -176,7 +181,6 @@ Module.prototype.handleRemoteData = function(handler) {
                     });
                 }
             }
-
             if (isSend) {
                 if (!self.isRecentData(dataObj.port, key, dataObj.data)) {
                     self.recentCheckData[dataObj.port] = {
@@ -294,13 +298,13 @@ Module.prototype.requestLocalData = function() {
 Module.prototype.initProperties = function(obj) {
     const allProperties = Object.getOwnPropertyNames(obj);
     allProperties.forEach(property => {
-        obj[property] = 0
+        obj[property] = 0;
     });
-}
+};
 
 /*
 // 하드웨어에서 온 데이터 처리
-패킷구조: ff 55 value_size value port type tailer a
+패킷구조: ff 55 value_size value port type tailer(a)
 value_size: Float면 2, Short면 3
 */
 Module.prototype.handleLocalData = function(data) {
@@ -308,7 +312,9 @@ Module.prototype.handleLocalData = function(data) {
     const datas = this.getDataByBuffer(data);
 
     datas.forEach((data) => {
-        if (data.length <= 4 || data[0] !== 255 || data[1] !== 85) { // Skip callOK from HW
+        console.log('fromHW: ' + data);
+        if (data.length <= 4 || data[0] !== 255 || data[1] !== 85) { // Check the header
+            console.log('Wrong header packet!!!');
             return;
         }
         const readData = data.subarray(2, data.length);
@@ -324,6 +330,7 @@ Module.prototype.handleLocalData = function(data) {
                 break;
             }
             default: {
+                console.log('Other data type is not handled yet!!!');
                 value = 0;
                 break;
             }
@@ -333,6 +340,11 @@ Module.prototype.handleLocalData = function(data) {
         const port = readData[readData.length - 2];
 
         switch (type) {
+            case self.sensorTypes.RESET: {
+                self.sensorData.RESET = value;
+                console.log('RESET successfully!');
+                break;
+            }
             case self.sensorTypes.DIGITAL: {
                 // this.initProperties(self.sensorData.DIGITAL)
                 self.sensorData.DIGITAL[port] = value;
@@ -351,21 +363,27 @@ Module.prototype.handleLocalData = function(data) {
                 self.sensorData.ULTRASONIC = value;
                 break;
             }
-            case self.sensorTypes.DHTTEMP: {
-                self.sensorData.DHTTEMP = value;
-                console.log(value)
-                break;
-            }
-            case self.sensorTypes.DHTHUMI: {
-                self.sensorData.DHTHUMI = value;
-                console.log(value)
-                break;
-            }
             case self.sensorTypes.TIMER: {
                 self.sensorData.TIMER = value;
                 break;
             }
+            case self.sensorTypes.DHTTEMP: {
+                self.sensorData.DHTTEMP = value;
+                console.log(value);
+                break;
+            }
+            case self.sensorTypes.DHTHUMI: {
+                self.sensorData.DHTHUMI = value;
+                console.log(value);
+                break;
+            }
+            case self.sensorTypes.IRREMOTE: {
+                self.sensorData.IRREMOTE = value;
+                // console.log(value);
+                break;
+            }
             default: {
+                console.log('No sensorTypes!!!');
                 break;
             }
         }
@@ -373,8 +391,8 @@ Module.prototype.handleLocalData = function(data) {
 };
 
 /*
-ff 55 len idx action device port (slot) (data) (tailer) (dummy)
-0  1  2   3   4      5      6    a      7      a        10Bytes
+ff 55 len idx action device port tailer (value) (dummy)
+0  1  2   3   4      5      6    a      2bytes  10Bytes
 len은 idx~데이터 까지의 길이 
 tailer는 HW에서 송신시 Serial.println()에 의한 LF값(10)
 idx은 아두이노 보드에서 실제 활용되지는 않음
@@ -382,66 +400,62 @@ idx은 아두이노 보드에서 실제 활용되지는 않음
 // 포트값(INPUT) 또는 구독 요청 만들기
 Module.prototype.makeSensorReadBuffer = function(device, port, data) {
     let buffer;
+    const value = new Buffer(2);
     const dummy = new Buffer([10]); // 10Bytes
-    if (device == this.sensorTypes.ULTRASONIC) {
-        buffer = new Buffer([
-            255,
-            85,
-            6,
-            sensorIdx,
-            this.actionTypes.GET,
-            device,
-            port[0],
-            port[1],
-            10, // tailer
-        ]);
-    } else if (device == this.sensorTypes.DHTTEMP) {
-        buffer = new Buffer([
-            255,
-            85,
-            5,
-            sensorIdx,
-            this.actionTypes.GET,
-            device,
-            port,
-            10,
-        ]);
-    } else if (device == this.sensorTypes.DHTHUMI) {
-        buffer = new Buffer([
-            255,
-            85,
-            5,
-            sensorIdx,
-            this.actionTypes.GET,
-            device,
-            port,
-            10,
-        ]);
-    } else if (!data) { // DigitalRead
-        buffer = new Buffer([
-            255,
-            85,
-            5,
-            sensorIdx,
-            this.actionTypes.GET,
-            device,
-            port,
-            10,
-        ]);
-    } else {
-        value = new Buffer(2);
-        value.writeInt16LE(data); // 2Bytes
-        buffer = new Buffer([
-            255,
-            85,
-            7,
-            sensorIdx,
-            this.actionTypes.GET,
-            device,
-            port,
-            10, // slot
-        ]);
-        buffer = Buffer.concat([buffer, value, dummy]);
+    switch (device) {
+        case this.sensorTypes.ULTRASONIC:
+            buffer = new Buffer([
+                255,
+                85,
+                6,
+                sensorIdx,
+                this.actionTypes.GET,
+                device,
+                port[0],
+                port[1],
+                10, // tailer
+            ]);
+            break;
+        case this.sensorTypes.DHTTEMP:
+        case this.sensorTypes.DHTHUMI:
+        case this.sensorTypes.IRREMOTE:
+            buffer = new Buffer([
+                255,
+                85,
+                5,
+                sensorIdx,
+                this.actionTypes.GET,
+                device,
+                port,
+                10,
+            ]);
+            break;
+        case this.sensorTypes.ANALOG: // AnalogRead
+            buffer = new Buffer([
+                255,
+                85,
+                5,
+                sensorIdx,
+                this.actionTypes.GET,
+                device,
+                port,
+                10,
+            ]);
+            break;
+        default:
+            value.writeInt16LE(data); // 2Bytes
+            buffer = new Buffer([
+                255,
+                85,
+                7,
+                sensorIdx,
+                this.actionTypes.GET,
+                device,
+                port,
+                10, 
+            ]);
+            buffer = Buffer.concat([buffer, value, dummy]);
+            break;
     }
     sensorIdx++;
     if (sensorIdx > 254) {
@@ -453,9 +467,9 @@ Module.prototype.makeSensorReadBuffer = function(device, port, data) {
 };
 
 /*
-ff   55   len idx action device port data     dummy
-0    1    2   3   4      5      6    7    8
-0xff 0x55 0x6 0x0 0x2    0xa    0x9  0x0  0x0 10Bytes
+ff   55   len idx action device port value  (etc) dummy
+0    1    2   3   4      5      6    7      8
+0xff 0x55 0x6 0x0 0x2    0xa    0x9  2bytes 0x0   10Bytes
 len은 idx~데이터 까지의 길이 
 */
 // 실행요청(OUTPUT) 만들기 
@@ -477,6 +491,8 @@ Module.prototype.makeOutputBuffer = function(device, port, data) {
             ]);
             buffer = Buffer.concat([buffer, value, dummy]);
             break;
+        case this.sensorTypes.IRRINIT:
+        case this.sensorTypes.DHTINIT:
         case this.sensorTypes.SERVO_PIN:
         case this.sensorTypes.DIGITAL:
         case this.sensorTypes.PWM: {
@@ -512,20 +528,6 @@ Module.prototype.makeOutputBuffer = function(device, port, data) {
                 port,
             ]);
             buffer = Buffer.concat([buffer, value, time, dummy]);
-            break;
-        }
-        case this.sensorTypes.DHTINIT:  {
-            value.writeInt16LE(data);
-            buffer = new Buffer([
-                255,
-                85,
-                6,
-                sensorIdx,
-                this.actionTypes.SET,
-                device,
-                port,
-            ]);
-            buffer = Buffer.concat([buffer, value, dummy]);
             break;
         }
         case this.sensorTypes.STEPPER: {
